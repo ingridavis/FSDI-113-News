5da6d917d4f0d27359d41765bf274e1b111e0cae
root <root@Sam_Asus.localdomain>
2020-11-02 23:43:57 -0800

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -133,8 +133,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 DEFAULT_FROM_EMAIL = 'ingrid.sam3@gmail.com'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
+
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
